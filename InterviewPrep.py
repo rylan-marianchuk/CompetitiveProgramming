aff7caf75d671b1344f167a7e0349ac348675957
Rylan Marianchuk <rylan.marianchuk@ucalgary.ca>
2020-01-30 16:02:03 -0700

    Added one file

---
+++ InterviewPrep.py
@@ -0,0 +1,13 @@
+
+A, B, T, t_a, t_b = list(map(input.split(), int))
+
+C = int(input())  # number of companies he is applying to
+
+C_data = []
+
+for comp in range(C):
+    l = list(map(input().split(), int))
+    C_data.append((l[0], l[1]))
+
+    
+
